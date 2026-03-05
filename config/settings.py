8620e275c2897f79c78a9c26661a088d59dc9248
ShohjahonIbrohimov <63698156+ShohjahonIbrohimov@users.noreply.github.com>
2026-03-05 22:47:28 +0500

    add server ip to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-q9k&7bhqpl@mr7vlapvvar%k(g#v!=_969u2$r#nng@=x$jz(i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dscc.opketme.uz', 'www.dscc.opketme.uz']
+ALLOWED_HOSTS = ['dscc.opketme.uz', 'www.dscc.opketme.uz', '157.180.85.173']
 
 LOGOUT_REDIRECT_URL = "home"
 
