3d767b4cb539f86027265b18f532f4e8b0b82ea5
ShohjahonIbrohimov <63698156+ShohjahonIbrohimov@users.noreply.github.com>
2026-03-05 22:25:35 +0500

    set allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q9k&7bhqpl@mr7vlapvvar%k(g#v!=_969u2$r#nng@=x$jz(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dscc.opketme.uz', 'www.dscc.opketme.uz']
 
 LOGOUT_REDIRECT_URL = "home"
 
